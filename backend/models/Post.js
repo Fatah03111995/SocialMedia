d8ab235b20e76e2357d33a2d785a10f311f22a58
Abdul Fatah <fatah03111995@gmail.com>
2023-02-07 21:24:56 +0700

    post

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,24 +1,33 @@
 import mongoose, { Schema } from 'mongoose';
 
-const postSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
+const postSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      types: Array,
+      default: [],
+    },
   },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  location: String,
-  description: String,
-  picturePath: String,
-  userPicturePath: String,
-  likes: {
-    type: Map,
-    of: Boolean,
-  },
-});
+  { timestamps: true }
+);
+
+export default mongoose.model('Posts', postSchema);
