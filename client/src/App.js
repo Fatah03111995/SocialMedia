33f926f48a1957794b07280e95093cf6823d57f5
Abdul Fatah <fatah03111995@gmail.com>
2023-02-19 18:46:35 +0700

    menambah theme

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,26 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/LoginPage';
 import ProfilePage from 'scenes/ProfilePage';
+import { createTheme } from '@mui/material';
+import { themeSettings } from 'theme';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
---
+++ client/src/lat.js
@@ -0,0 +1,16 @@
+const themeSEttings = (mode) => {
+    return {
+        pallete: {
+            ...(mode==='dark' ? {
+                primary:{
+                    dark, medium, SVGFESpotLightElement
+                },
+                neutral:
+                background:,
+            }:{
+                //light
+            })
+        }
+        typhography
+    }
+}
